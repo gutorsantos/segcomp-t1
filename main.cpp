6e414447a854e06828ce1b181844e3b98f9e34ab
Gustavo Santos <gustavo.r.santos@outlook.com>
2021-07-28 14:25:53 -0300

    encrypt/decrypt done

---
+++ main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+string gen_keystream(string key, string msg) {
+    string keystream;
+    int flag = 0;
+    for(int i = 0; i < msg.size(); i++) {
+        if((int)msg[i] >= (int)'A' &&  (int)msg[i] <= (int)'Z')
+            keystream.push_back(key[(i-flag) % key.size()]);
+        else
+            flag++;
+    }
+    cout << "KEYSTREAM: ";
+    cout << keystream << endl;
+    return keystream;
+}
+
+string insert_key() {
+    string key;
+    cout << "Insert the key: ";
+    cin >> key;
+    return key;
+}
+
+string insert_message() {
+    string message;
+    cout << "Insert the message: ";
+    cin.ignore();
+    getline(cin, message);
+    return message;
+}
+
+string get_decrypt() {
+    string key = insert_key();
+    string encrypted = insert_message();
+    string keystream = gen_keystream(key, encrypted);
+    
+    string decrypted;
+    int flag = 0;
+    for(int i = 0; i < encrypted.size(); i++) {
+        if((int)encrypted[i] >= (int)'A' &&  (int)encrypted[i] <= (int)'Z') {
+            char c_new = (char)(((int) ((encrypted[i] - 'A')-(keystream[i-flag] - 'A')+26)) % 26 + 'A');
+            decrypted.push_back(c_new);
+        }else {
+            decrypted.push_back(encrypted[i]);
+            flag++;
+        }
+    }
+    cout << decrypted << endl;
+
+    return key;
+}
+
+
+string get_encrypt() {
+    string key = insert_key();
+    string original = insert_message();
+    string keystream = gen_keystream(key, original);
+    
+    string encrypted;
+    int flag = 0;
+    for(int i = 0; i < original.size(); i++) {
+        if((int)original[i] >= (int)'A' &&  (int)original[i] <= (int)'Z') {
+            char c_new = (char)(((int) ((original[i] - 'A')+(keystream[i-flag] - 'A'))) % 26 + 'A' );
+            encrypted.push_back(c_new);
+        }else {
+            encrypted.push_back(original[i]);
+            flag++;
+        }
+    }
+    cout << encrypted << endl;
+
+    return key;
+}
+
+int main() {
+    int opt = 0;
+    printf("Vigenere Chyper\n1. Encryptor\n2. Decryptor\n");
+    do {
+        printf("Choose an option: ");
+        cin >> opt;
+        switch(opt){
+            case 1:
+                get_encrypt();
+                break;
+            case 2:
+                get_decrypt();
+                break;
+            default:
+                printf("Invalid option!\n");
+                break;
+        }
+    } while(opt < 1 || opt > 2);
+    return 0;
+}
